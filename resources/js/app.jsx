1262d0e3b5f7c8d3d33a5d90796a5495a4e82760
Dec4rx <decarwar@gmail.com>
2024-05-14 18:52:54 -0600

    Initial configuration

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import Example from './components/Example';
+import React from 'react';
+
+
+ReactDOM.createRoot(document.getElementById('application')).render(<Example />);
